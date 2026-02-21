abb4ce57444905052b8cf7a60ef13dbdc30a96f9
kadempally sai krish karthik <kadempally.krish0310@gmail.com>
2026-02-21 22:52:14 +0530

    Create number_guessing_game.cpp
    Wrote the basic structure of the number guessing game.
    Learned how to generate random numbers using rand() and srand().

---
+++ Task-1_Number-Guessing-Game/number_guessing_game.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int secretNumber, guess, attempts = 0;
+
+   
+    srand(time(0));
+    secretNumber = rand() % 100 + 1;
+
+    cout << "...... Number Guessing Game ......" << endl;
+    cout << "I have selected a number between 1 and 100." << endl;
+    cout << "Try to guess it!" << endl << endl;
+
+   
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } 
+        else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } 
+        else {
+            cout << "\ncorrect" << endl;
+            cout << "You guessed the correct number in "
+                 << attempts << " attempts." << endl;
+        }
+
+    } while (guess != secretNumber);
+  
+    cout << "================================" << endl;
+
+    return 0;
+}
